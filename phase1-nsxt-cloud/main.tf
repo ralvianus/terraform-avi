d5ce80efa9f1c2c4aff1b630460b3ef548933ecf
Robert Alvianus <robert@alvianus.net>
2022-03-04 21:48:54 +0800

    update..

--- phase1-nsxt-cloud/main.tf
+++ phase1-nsxt-cloud/main.tf
@@ -185,7 +185,11 @@ resource "avi_cloud" "nsxt_cloud" {
       management_network_config {
         tz_type = var.nsxt_cloud_mgmt_tz_type
         transport_zone = data.nsxt_transport_zone.nsxt_mgmt_tz_name.id
-        overlay_segment = var.nsxt_mgmt_segment_id
+        tier1_lrs {
+          tier1_lr_id = var.nsxt_cloud_lr1
+          segment_id  = var.nsxt_mgmt_segment_id
+        }
+        # overlay_segment = var.nsxt_mgmt_segment_id
       }
       data_network_config {
         tz_type = var.nsxt_cloud_mgmt_tz_type
