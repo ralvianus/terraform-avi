143797bdb7a65fe89b6febcee8af7e2114da093d
Robert Alvianus <robert@alvianus.net>
2022-03-04 23:04:55 +0800

    update..

--- phase1-nsxt-cloud/main.tf
+++ phase1-nsxt-cloud/main.tf
@@ -248,7 +248,8 @@ resource "avi_serviceenginegroup" "cmp-se-group" {
 	vcenters {
 		vcenter_ref = var.nsxt_cloud_vcenter_name
     nsxt_clusters {
-      include = data.vsphere_compute_cluster.cmp.id
+      cluster_ids = data.vsphere_compute_cluster.cmp.id
+      include = true
     }
 	}
 }
@@ -265,7 +266,8 @@ resource "avi_serviceenginegroup" "mgmt-se-group" {
   vcenters {
 		vcenter_ref = var.nsxt_cloud_vcenter_name
     nsxt_clusters {
-      include = data.vsphere_compute_cluster.mgmt.id
+      cluster_ids = data.vsphere_compute_cluster.mgmt.id
+      include = true
     }
 	}
 }
